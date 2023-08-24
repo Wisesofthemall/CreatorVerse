a2f45630693687a05b0a312fcf879f26b05c50b5
Wisesofthemall <foxxydieujuste@gmail.com>
2023-08-24 12:04:07 -0400

    feat: Added View/Add Creator buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import "./App.css";
-
+import CustomButton from "./components/inputs/CustomButton.tsx";
 function App() {
   return (
-    <div className="Header grid place-items-center h-80">CreatorVerse</div>
+    <div className="">
+      <div className="text-2xl  md:text-6xl lg:text-8xl text-stone-50 grid place-items-center h-80">
+        CreatorVerse
+      </div>
+      <div className="text-stone-50 text-center mb-11">
+        Find all your favorite creator's social here!
+      </div>
+      <div className="flex justify-evenly ">
+        <CustomButton title="View All Creators" />
+        <CustomButton title="Add a Creator" />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/inputs/CustomButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  title: string;
+};
+
+function CustomButton({ title }: Props) {
+  return (
+    <button className="text-stone-50 font-bold bg-blue-600 rounded-md w-max">
+      {title}
+    </button>
+  );
+}
+
+export default CustomButton;
